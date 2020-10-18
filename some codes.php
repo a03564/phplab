8f3e70a3f16c6818a566fa44a4529ffe86f07cd4
a03564 <73059052+a03564@users.noreply.github.com>
2020-10-18 18:17:39 +0800

    Added new feature
    fixed some stuff

--- some codes.php
+++ some codes.php
@@ -44,7 +44,8 @@ background-color:#dddddd;
 <p class="new">This is another paragraph.</p> 
 <p class="new">This is a new paragraph.</p>
 
-<?php echo "hello new world <br>"; />
+<?php echo "hello new world <br>"; ?>
+//fixing some simple syntaxes
 
 </body>
 </html>

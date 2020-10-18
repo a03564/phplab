92d49cdf4a91b6e870924916faaac8c56393b53b
a03564 <73059052+a03564@users.noreply.github.com>
2020-10-18 18:12:40 +0800

    This is the first cut
    With some basic codes

---
+++ some codes.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  background-color:#d0e4fe; 
+}
+
+h1 {
+  color: blue;
+  text-align: center;
+}
+
+h1#secondheading {
+color:#ff0000; 
+}
+
+p {
+  font-family:"Times New Roman";
+  font-size: 30px;
+  color:#0000ff
+}
+
+p1 {
+  font-family:"Verdana";
+  font-size: 20px;
+  color:#0000ff
+}
+
+p.new  {
+background-color:#dddddd;
+}
+
+</style>
+</head>
+<body>
+
+<h1>My First CSS Example</h1>
+<h1>CSS is so fun!</h1>
+<h1 id="secondheading">CSS is interesting!</h1>
+<p>This is a paragraph.</p>
+<p1>This is another paragraph. </p1>
+
+<p class="new">This is another paragraph.</p> 
+<p class="new">This is a new paragraph.</p>
+
+<?php echo "hello new world <br>"; />
+
+</body>
+</html>
